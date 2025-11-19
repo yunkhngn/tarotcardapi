e321c093238547aeb40e7b07339ebdc12064eb1e
yunkhngn <rogkhoa.mail@gmail.com>
2025-11-20 04:10:16 +0700

    chore: adding HeroUI

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
+import { HeroUIProvider } from '@heroui/react';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <HeroUIProvider>
+      <Component {...pageProps} />
+    </HeroUIProvider>
+  );
 }
 
